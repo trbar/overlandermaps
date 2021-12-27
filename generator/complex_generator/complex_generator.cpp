organicmaps/organicmaps

[ios][android][desktop] Add application version to Platform
Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

generator/complex_generator/complex_generator.cpp
@@ -42,7 +42,6 @@
 #include <exception>
 #include <iostream>
 
-#include "build_version.hpp"
 #include "defines.hpp"
 
 #include "3party/gflags/src/gflags/gflags.h"
@@ -61,15 +60,14 @@ DEFINE_bool(debug, false, "Debug mode.");
 MAIN_WITH_ERROR_HANDLING([](int argc, char ** argv) {
   CHECK(IsLittleEndian(), ("Only little-endian architectures are supported."));
 
+  Platform & pl = GetPlatform();
   google::SetUsageMessage(
       "complex_generator is a program that generates complexes on the basis of "
       "the last generation of maps. Complexes are a hierarchy of interesting "
       "geographical features.");
-  google::SetVersionString(std::to_string(omim::build_version::git::kTimestamp) + " " +
-                           omim::build_version::git::kHash);
+  google::SetVersionString(pl.Version());
   google::ParseCommandLineFlags(&argc, &argv, true);
 
-  Platform & pl = GetPlatform();
   auto threadsCount = pl.CpuCores();
   CHECK(!FLAGS_user_resource_path.empty(), ());
   pl.SetResourceDir(FLAGS_user_resource_path);

generator/generator_tool/generator_tool.cpp
@@ -70,8 +70,6 @@
 
 #include "3party/gflags/src/gflags/gflags.h"
 
-#include "build_version.hpp"
-
 using namespace std;
 
 namespace
@@ -213,13 +211,12 @@ MAIN_WITH_ERROR_HANDLING([](int argc, char ** argv)
 {
   CHECK(IsLittleEndian(), ("Only little-endian architectures are supported."));
 
+  Platform & pl = GetPlatform();
   google::SetUsageMessage(
       "Takes OSM XML data from stdin and creates data and index files in several passes.");
-  google::SetVersionString(std::to_string(omim::build_version::git::kTimestamp) + " " +
-                           omim::build_version::git::kHash);
+  google::SetVersionString(pl.Version());
   google::ParseCommandLineFlags(&argc, &argv, true);
 
-  Platform & pl = GetPlatform();
   unsigned threadsCount = FLAGS_threads_count != 0 ? static_cast<unsigned>(FLAGS_threads_count)
                                                    : pl.CpuCores();
 

trbar/overlandermaps

generator/complex_generator/complex_generator.cpp
@@ -42,7 +42,6 @@
 #include <exception>
 #include <iostream>
 
-#include "build_version.hpp"
 #include "defines.hpp"
 
 #include "gflags/gflags.h"
@@ -61,15 +60,14 @@ DEFINE_bool(debug, false, "Debug mode.");
 MAIN_WITH_ERROR_HANDLING([](int argc, char ** argv) {
   CHECK(IsLittleEndian(), ("Only little-endian architectures are supported."));
 
+  Platform & pl = GetPlatform();
   gflags::SetUsageMessage(
       "complex_generator is a program that generates complexes on the basis of "
       "the last generation of maps. Complexes are a hierarchy of interesting "
       "geographical features.");
-  gflags::SetVersionString(std::to_string(omim::build_version::git::kTimestamp) + " " +
-                           omim::build_version::git::kHash);
+  gflags::SetVersionString(pl.Version());
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  Platform & pl = GetPlatform();
   auto threadsCount = pl.CpuCores();
   CHECK(!FLAGS_user_resource_path.empty(), ());
   pl.SetResourceDir(FLAGS_user_resource_path);

generator/generator_tool/generator_tool.cpp
@@ -70,8 +70,6 @@
 
 #include "gflags/gflags.h"
 
-#include "build_version.hpp"
-
 using namespace std;
 
 namespace
@@ -213,13 +211,12 @@ MAIN_WITH_ERROR_HANDLING([](int argc, char ** argv)
 {
   CHECK(IsLittleEndian(), ("Only little-endian architectures are supported."));
 
+  Platform & pl = GetPlatform();
   gflags::SetUsageMessage(
       "Takes OSM XML data from stdin and creates data and index files in several passes.");
-  gflags::SetVersionString(std::to_string(omim::build_version::git::kTimestamp) + " " +
-                           omim::build_version::git::kHash);
+  gflags::SetVersionString(pl.Version());
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
-  Platform & pl = GetPlatform();
   unsigned threadsCount = FLAGS_threads_count != 0 ? static_cast<unsigned>(FLAGS_threads_count)
                                                    : pl.CpuCores();
 